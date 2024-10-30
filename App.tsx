465d2113095231880bf81880439df958d1d1bccb
Elgraabi <silvatdb8@gmail.com>
2024-10-30 16:31:59 -0300

    feat: Reenstruturando pastas do projeto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Input from './src/components/inputs/input';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Input title="" iconName="person" placeholder="Digite seu usuário" />
       <StatusBar style="auto" />
     </View>
   );
