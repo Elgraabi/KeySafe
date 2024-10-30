128fb6e93766abbdbb34d81a012574f0c816e546
Elgraabi <silvatdb8@gmail.com>
2024-10-30 20:43:44 -0300

    feat: Componente button e styles

---
+++ src/components/button/button.tsx
@@ -0,0 +1,16 @@
+import { View, Button, Text, TouchableOpacityProps, TouchableOpacity } from "react-native";
+import styles from "./styles";
+
+type ButtonProps = TouchableOpacityProps & {
+    title: string;
+}
+
+export default function button({ title }: ButtonProps) {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>{title}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
