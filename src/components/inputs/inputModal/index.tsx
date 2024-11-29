a48731d40458c4b081609e4f7e235fe0c2fcf0a9
Elgraabi <silvatdb8@gmail.com>
2024-11-28 22:37:23 -0300

    update: adicionando placeHolder e defaultValue opcional para inputModal

--- src/components/inputs/inputModal/index.tsx
+++ src/components/inputs/inputModal/index.tsx
@@ -4,15 +4,17 @@ import styles from "./styles";
 
 type InputProps = TextInputProps & {
     iconName: string;
-    placeHolder: string;
+    placeHolder?: string;
+    defaultValue?: string;
 };
 
-export default function InputModal({ iconName, placeHolder, ...rest }: InputProps) {
+export default function InputModal({ iconName, placeHolder, defaultValue, ...rest }: InputProps) {
     return (
         <View style={styles.container}> 
             <TextInput 
                 style={styles.inputText}
-                defaultValue={placeHolder}
+                defaultValue={defaultValue}
+                placeholder={placeHolder}
             />
             <Icon name={iconName} size={20} color="#3C7DC3"/>
         </View>
