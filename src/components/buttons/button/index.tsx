12f72d4c45ca113b6e3623dab91bf567a25cdc01
Henrique Santos <henrsilvasantos@gmail.com>
2024-12-01 14:51:14 -0300

    Upgrade no perfil do usuario

--- src/components/buttons/button/index.tsx
+++ src/components/buttons/button/index.tsx
@@ -1,38 +1,57 @@
-import { View, Text, TouchableOpacityProps, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacityProps,
+  TouchableOpacity,
+} from "react-native";
 import styles from "./styles";
 
-type StyleKeys = 'loading' | 'return' | 'enter' 
-| 'forgotYourPassword' | 'register' | 'confirm' | 'cancel' | 'registerUser' | 'save' | 'delet' | 'cancelModal';
+type StyleKeys =
+  | "loading"
+  | "return"
+  | "enter"
+  | "forgotYourPassword"
+  | "register"
+  | "confirm"
+  | "cancel"
+  | "registerUser"
+  | "save"
+  | "delet"
+  | "cancelModal"
+  | "alterar"
+  | "editarPerf";
 
 type ButtonProps = TouchableOpacityProps & {
-    title?: string;
-    className: StyleKeys;
-}
+  title?: string;
+  className: StyleKeys;
+};
 
 export default function Button({ title, className, ...rest }: ButtonProps) {
-    let styleText;
+  let styleText;
 
-    if (className === 'forgotYourPassword') {
-        styleText = { ...styles.forgotText };
-    } else if (className === 'register') {
-        styleText = { ...styles.forgotText };
-    } else if (className === 'registerUser') {
-        styleText = { ...styles.registerUserText };
-    } else if (className === 'save') {
-        styleText = { ...styles.saveButtonText };
-    } else if (className === 'delet') {
-        styleText = { ...styles.deletButtonText };
-    } else if (className === 'cancelModal') {
-        styleText = { ...styles.textCancelModal};
-    } else {
-        styleText = { ...styles.buttonText };
-    }
+  if (className === "forgotYourPassword") {
+    styleText = { ...styles.forgotText };
+  } else if (className === "register") {
+    styleText = { ...styles.forgotText };
+  } else if (className === "registerUser") {
+    styleText = { ...styles.registerUserText };
+  } else if (className === "save") {
+    styleText = { ...styles.saveButtonText };
+  } else if (className === "delet") {
+    styleText = { ...styles.deletButtonText };
+  } else if (className === "cancelModal") {
+    styleText = { ...styles.textCancelModal };
+  } else if (className === "alterar") {
+    styleText = { ...styles.textAlterar };
+  } else {
+    styleText = { ...styles.buttonText };
+  }
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity style={[styles[className], styles.button]} {...rest}>
-                <Text style={styleText}>{title}</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={[styles[className], styles.button]} {...rest}>
+        <Text style={styleText}>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
