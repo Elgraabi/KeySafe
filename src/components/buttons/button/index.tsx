906bd8abe56194e0637e04d9dc6350b577561f06
Henrique Santos <henrsilvasantos@gmail.com>
2024-12-01 01:24:50 -0300

    Não subir essas alterações

--- src/components/buttons/button/index.tsx
+++ src/components/buttons/button/index.tsx
@@ -1,36 +1,54 @@
-import { View, Text, TouchableOpacityProps, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacityProps,
+  TouchableOpacity,
+} from "react-native";
 import styles from "./styles";
 
-type StyleKeys = 'loading' | 'return' | 'enter' 
-| 'forgotYourPassword' | 'register' | 'confirm' | 'cancel' | 'registerUser' | 'save' | 'delet';
+type StyleKeys =
+  | "loading"
+  | "return"
+  | "enter"
+  | "forgotYourPassword"
+  | "register"
+  | "confirm"
+  | "cancel"
+  | "registerUser"
+  | "save"
+  | "delet"
+  | "alterar"
+  | "edit";
 
 type ButtonProps = TouchableOpacityProps & {
-    title?: string;
-    className: StyleKeys;
-}
+  title?: string;
+  className: StyleKeys;
+};
 
 export default function Button({ title, className, ...rest }: ButtonProps) {
-    let styleText;
+  let styleText;
 
-    if (className === 'forgotYourPassword') {
-        styleText = { ...styles.forgotText };
-    } else if (className === 'register') {
-        styleText = { ...styles.forgotText };
-    } else if (className === 'registerUser') {
-        styleText = { ...styles.registerUserText };
-    } else if (className === 'save') {
-        styleText = { ...styles.saveButtonText };
-    } else if (className === 'delet') {
-        styleText = { ...styles.deletButtonText };
-    } else {
-        styleText = { ...styles.buttonText };
-    }
+  if (className === "forgotYourPassword") {
+    styleText = { ...styles.forgotText };
+  } else if (className === "register") {
+    styleText = { ...styles.forgotText };
+  } else if (className === "registerUser") {
+    styleText = { ...styles.registerUserText };
+  } else if (className === "save") {
+    styleText = { ...styles.saveButtonText };
+  } else if (className === "delet") {
+    styleText = { ...styles.deletButtonText };
+  } else if (className === "alterar") {
+    styleText = { ...styles.alterarButtonText };
+  } else {
+    styleText = { ...styles.buttonText };
+  }
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity style={[styles[className], styles.button]} {...rest}>
-                <Text style={styleText}>{title}</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={[styles[className], styles.button]} {...rest}>
+        <Text style={styleText}>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
--- src/screens/editProfile/index.tsx
+++ src/screens/editProfile/index.tsx
@@ -4,18 +4,69 @@ import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RoutesParams } from "../../navigation/routesParams";
+import ButtonCircle from "../../components/buttons/buttonCircle";
+import Input from "../../components/inputs/input";
+import Button from "../../components/buttons/button";
 
-type EditProfileParamsList = NativeStackNavigationProp<RoutesParams, "EditProfile">;
+type EditProfileParamsList = NativeStackNavigationProp<
+  RoutesParams,
+  "EditProfile"
+>;
 
 export default function EditProfileScreen() {
-    const navigation = useNavigation<EditProfileParamsList>();
+  const navigation = useNavigation<EditProfileParamsList>();
 
-    return (
-        <View style={styles.container}>
-            <Image
-                style={styles.image}
-                source={require("../../../assets/imagens/logo.png")}
-            />
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../../../assets/imagens/logo.png")}
+      />
+
+      <View style={styles.viewText}>
+        <Text style={styles.textText}>KeySafe</Text>
+      </View>
+
+      <View style={styles.viewTitle}>
+        <Text style={styles.textTitle}>Meu perfil</Text>
+      </View>
+
+      <View style={styles.row}>
+        <ButtonCircle
+          className="return"
+          iconName="arrow-left"
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+      <Input title="Nome completo" iconName="user" />
+      <Input title="E-mail" iconName="envelope-o" />
+      <Input title="E-mail de recuperação" iconName="envelope-o" />
+      <Input title="Senha" iconName="lock" secureTextEntry={true} />
+
+      <View style={styles.view}>
+        <Button
+          title="Alterar senha"
+          className="alterar"
+          onPress={() => navigation.goBack()}
+          style={styles.buttonAlterar} // Estilo do botão Alterar senh
+        />
+        <Button
+          title="Editar perfil"
+          className="edit"
+          onPress={() => console.log("Usuário cadastrado com sucesso")}
+          style={styles.buttonEdit} // Estilo do botão Editar perfil
+        />
+
+        {/* Botão "Excluir perfil" movido para uma nova View */}
+        <View style={styles.bottomButtonContainer}>
+          <Button
+            title="Excluir perfil"
+            className="delet"
+            onPress={() => console.log("Usuário cadastrado com sucesso")}
+            style={styles.buttonExcluir}
+          />
         </View>
-    );
+      </View>
+    </View>
+  );
 }
